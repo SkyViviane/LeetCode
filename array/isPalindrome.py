5dc41fec44cc401d6f2621c4c66f4d31e6440bba
SkyViviane <sky.zhangtingting@yahoo.com>
2019-08-12 15:41:51 +0800

    Create isPalindrome.py

---
+++ array/isPalindrome.py
@@ -0,0 +1,39 @@
+"""
+判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+示例 1:
+输入: 121
+输出: true
+
+示例 2:
+输入: -121
+输出: false
+解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+
+示例 3:
+输入: 10
+输出: false
+解释: 从右向左读, 为 01 。因此它不是一个回文数。
+
+"""
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        res = 0
+        k = x
+        while k > res:
+            res = res * 10 + k % 10
+            k //= 10
+        return res == k or res // 10 == k
+
+
+so = Solution()
+x = 12321
+print(so.isPalindrome(x))
