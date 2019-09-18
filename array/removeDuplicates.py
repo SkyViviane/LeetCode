6b87648fcfc312e6b9d865c1cf492bcc16a42695
SkyViviane <sky.zhangtingting@yahoo.com>
2019-09-18 16:51:46 +0800

    Update removeDuplicates.py

--- array/removeDuplicates.py
+++ array/removeDuplicates.py
@@ -29,13 +29,6 @@ for (int i = 0; i < len; i++) {
 
 """
 
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-
 class Solution(object):
     def removeDuplicates(self, nums):
         """
