c896bf9025ebbb92eb9b4070c86763b19ba3b1fc
SkyViviane <sky.zhangtingting@yahoo.com>
2019-11-18 15:57:36 +0800

    Create numDecodings.py

---
+++ DynamicPlanning/numDecodings.py
@@ -0,0 +1,65 @@
+'''
+一条包含字母 A-Z 的消息通过以下方式进行了编码：
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+给定一个只包含数字的非空字符串，请计算解码方法的总数。
+
+示例 1:
+输入: "12"
+输出: 2
+解释: 它可以解码为 "AB"（1 2）或者 "L"（12）。
+
+示例 2:
+输入: "226"
+输出: 3
+解释: 它可以解码为 "BZ" (2 26), "VF" (22 6), 或者 "BBF" (2 2 6) 。
+'''
+
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s or s[0] == '0':
+            return 0
+        dp = [0] * len(s)
+        dp[0] = 1
+        if len(s) > 1:
+            if s[1] == '0':
+                if int(s[0:2]) <= 26:
+                    dp[1] = 1
+                else:
+                    return 0
+            else:
+                if int(s[0:2]) <= 26:
+                    dp[1] = 2
+                else:
+                    dp[1] = 1
+        else:
+            return 1
+
+        for i in range(2, len(s)):
+            if s[i] == '0':
+                if s[i - 1] == '0':
+                    return 0
+                elif int(s[i - 1:i + 1]) <= 26:
+                    dp[i] = dp[i - 2]
+                else:
+                    return 0
+            else:
+                if s[i - 1] == '0':
+                    dp[i] = dp[i - 1]
+                elif int(s[i - 1:i + 1]) <= 26:
+                    dp[i] = dp[i - 1] + dp[i - 2]
+                else:
+                    dp[i] = dp[i - 1]
+        return dp[len(s) - 1]
+
+if __name__ == '__main__':
+    so = Solution()
+    s = "11"
+    print(so.numDecodings(s))
