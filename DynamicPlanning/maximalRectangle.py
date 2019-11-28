9eda3066daf7270d092252c05fb129a042e68b2e
SkyViviane <sky.zhangtingting@yahoo.com>
2019-11-28 15:23:29 +0800

    Create maximalRectangle.py

---
+++ DynamicPlanning/maximalRectangle.py
@@ -0,0 +1,68 @@
+'''
+给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+
+示例:
+
+输入:
+[
+  ["1","0","1","0","0"],
+  ["1","0","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","0","0","1","0"]
+]
+输出: 6
+
+'''
+
+
+class Solution(object):
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix:
+            return 0
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        left = [0] * n
+        right = [n] * n
+        height = [0] * n
+        maxarea = 0
+
+        for i in range(m):
+
+            cur_left, cur_right = 0, n
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    height[j] += 1
+                else:
+                    height[j] = 0
+
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    left[j] = max(left[j], cur_left)
+                else:
+                    left[j] = 0
+                    cur_left = j + 1
+
+            for j in range(n - 1, -1, -1):
+                if matrix[i][j] == '1':
+                    right[j] = min(right[j], cur_right)
+                else:
+                    right[j] = n
+                    cur_right = j
+
+            for j in range(n):
+                maxarea = max(maxarea, height[j] * (right[j] - left[j]))
+
+        return maxarea
+
+
+if __name__ == '__main__':
+    so = Solution()
+    s = [["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"],
+         ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]]
+    print(so.maximalRectangle(s))
